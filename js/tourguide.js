cbaae73a5a4035baa028071d20c028c04543250d
zombiehugs <gerdsen@gmail.com>
2014-01-15 12:21:10 -0600

    Initial Commit, demo page unfinished.

---
+++ js/tourguide.js
@@ -0,0 +1,206 @@
+﻿//**********************************************************************************
+// TourGuide Class
+// John Gerdsen
+// 1/28/12
+//
+// Take an array of commands and elements and traverse through
+// to display to the user the proper actions and flow of page and
+// controls.
+//
+//**********************************************************************************
+
+$.Class('TourGuide',
+
+/* @static */
+        {
+        callbackUrl: null,
+        callbackMethod: null,
+        stepTimer: null,
+        stepTimerTimeout: null,
+        currentIndex: 0,
+        isPlaying: false,
+        currentElement: null,
+        currentPosition: null,
+        numberOfSteps: null,
+        fxSpeed: 750,
+        buttonText: null,
+        position: null,
+        direction: null,
+        currentText: null,
+        buttonId: null,
+        speechAmplitude: 125,
+        speechPitch: 45,
+        speechSpeed: 76,
+        speechGap: 2
+    },
+
+/* @dynamic/prototype */
+        {
+
+        init: function (position, buttonText, instructions) {
+            // initialize with instructions sent from call
+            this.instructions = instructions;
+            this.currentIndex = 0;
+            this.position = position;
+            this.buttonText = buttonText;
+            this.direction = (this.position == 'top') ? "up" : "down";
+            this.numberOfSteps = instructions.length - 1;
+            this.createUI();
+            // add necessary DOM to page
+        },
+
+        reset: function () {
+            //Reset DOM and JS to beginning
+            clearTimeout(this.stepTimer);
+            this.currentIndex = 0;
+            this.currentPosition = null;
+            this.currentElement = null;
+            $("#stepTitle").html("");
+            $("#stepInstruction").html("");
+            $('#highlightOverlay').css({ top: 0, left: 0 });
+        },
+        dump: function (toDump) {
+            // dump instructions array to screen
+            //alert(varDump(this.instructions));
+            alert(varDump(toDump));
+        },
+
+        start: function () {
+            // start tour and call until array is traversed completely
+            this.currentElement = this.instructions[this.currentIndex][0];
+            this.currentPosition = $(this.currentElement).offset();
+            $("#tourBox").animate({ top: (this.position == 'top') ? '-5' : 'null', bottom: (this.position == 'bottom') ? '-2' : 'null' }, this.fxSpeed);
+            this.show();
+            //this.dump(this.currentPosition);
+        },
+
+        show: function () {
+            $("#stepTitle").html(this.instructions[this.currentIndex][1]);
+            $("#stepInstruction").html(this.instructions[this.currentIndex][2]);
+            $('#highlightOverlay').width($(this.currentElement).outerWidth());
+            $('#highlightOverlay').height($(this.currentElement).outerHeight());
+            $('#highlightOverlay').css({ top: this.currentPosition.top, left: this.currentPosition.left });
+            $('#highlightOverlay').show("puff", { percent: 150 }, this.fxSpeed, this.doWeContinue());
+        },
+
+        hide: function () {
+            $("#tourBox").animate({ top: (this.position == 'top') ? '-172' : 'null', bottom: (this.position == 'bottom') ? '-172' : 'null' }, this.fxSpeed);
+            $('#highlightOverlay').hide("puff", { percent: 150 }, 500);
+            this.reset();
+        },
+
+        dictate: function () {
+            //Using eSpeak Javascript Port created by https://github.com/kripken/speak.js
+            speak(this.instructions[this.currentIndex][2], { amplitude: this.speechAmplitude, wordgap: this.speechGap, pitch: this.speechPitch, speed: this.speechSpeed, noWorker: true });
+        },
+
+        doWeContinue: function () {
+            //alert(this.currentIndex);
+            if (this.isPlaying) {
+                //TODO Clean up these references in timeout.
+                var instance = this;
+                this.stepTimer = setTimeout(function () {
+                    instance.next();
+                }, this.instructions[this.currentIndex][3]);
+            }
+        },
+
+        next: function () {
+            // advance slide
+            if (this.currentIndex < this.numberOfSteps) {
+                this.currentIndex++;
+                this.resume();
+            }
+        },
+
+        previous: function () {
+            // reverse slide
+            if (this.currentIndex >= 1) {
+                this.currentIndex--;
+                this.resume();
+            }
+        },
+
+        stop: function () {
+            // clear timeout and cancel all actions and set all elements back to original
+        },
+
+        pause: function () {
+            // clear current timeout and wait for user input
+            this.isPlaying = false;
+            clearTimeout(this.stepTimer);
+        },
+
+        play: function () {
+            this.isPlaying = true;
+            this.start();
+        },
+
+        resume: function () {
+            this.currentElement = this.instructions[this.currentIndex][0];
+            this.currentPosition = $(this.currentElement).offset();
+            this.show();
+        },
+
+        createUI: function (position, buttonText) {
+            // call to tourguidehelper js
+            this.assembleDOM();
+            this.addTourButton();
+            this.addEvents();
+            centerBox();
+        },
+
+        assembleDOM: function () {
+            // create any necessary UI elements for tour
+            var $tourGuideDOM = $('<div id="tourBox"><div id="tourContent"><div id="controlClose"><img src="img/close16x16.png" width="16" height="16" alt="Close Tour" /></div><div id="stepContent"><div id="stepTitle" class="tk-facitweb"></div><div class="dictateBtn" id="dictateTour"><img src="img/speech.png" width="24" height="24" alt="Dictate" /></div><div id="stepInstruction" class="tk-facitweb"></div></div><div id="tourControls"><div id="itemContainer"><div class="controlItem" id="rwTour"><img src="img/rw32x32.png" width="24" height="24" alt="Rewind" /></div><div class="controlItem" id="psTour"><img src="img/pause32x32.png" width="24" height="24" alt="Pause" /></div><div class="controlItem" id="plTour"><img src="img/play32x32.png" width="24" height="24" alt="Play" /></div><div class="controlItem" id="ffTour"><img src="img/ff32x32.png" width="24" height="24" alt="Skip" /></div></div></div></div></div>');
+            var $highlightOverlay = $('<div id="highlightOverlay"></div>');
+            $('body').append($tourGuideDOM);
+            (this.position == 'top') ? $('#tourBox').css('top', -172) : $('#tourBox').css('bottom', -172);
+            $('body').append($highlightOverlay);
+            $('body').append("<div id=\"audio\"></div>");
+        },
+
+        addTourButton: function (position, buttonText) {
+            var $tourButton = $('<div id="tourBtn"></div>');
+            (this.position == 'top') ? $($tourButton).appendTo('#tourBox') : $($tourButton).prependTo('#tourBox');
+            $('#tourBtn').html(this.buttonText);
+        },
+
+        addEvents: function () {
+            //Bind onClick to Close Button, we pass reference of this class to event handler
+            $('#controlClose').bind('click', { instance: this }, function (event) {
+                event.data.instance.hide();
+            });
+            $('#ffTour').bind('click', { instance: this }, function (event) {
+                event.data.instance.next();
+            });
+            $('#rwTour').bind('click', { instance: this }, function (event) {
+                event.data.instance.previous();
+            });
+            $('#plTour').bind('click', { instance: this }, function (event) {
+                event.data.instance.play();
+            });
+            $('#psTour').bind('click', { instance: this }, function (event) {
+                event.data.instance.pause();
+            });
+            $('#dictateTour').bind('click', { instance: this }, function (event) {
+                event.data.instance.dictate();
+            });
+            $('#tourBtn').bind('click', { instance: this }, function (event) {
+                event.data.instance.start();
+            });
+        },
+
+        dispose: function () {
+            // remove all traces
+            this.callbackUrl = null;
+            this.callbackMethod = null;
+            this.stepTimer = null;
+            this.currentIndex = null;
+            this.isPlaying = false;
+            this.currentElement = null;
+            this.currentPosition = null;
+            this.numberOfSteps = null;
+            this.fxSpeed = null;
+        }
+    });
---
+++ js/tourguidehelper.js
@@ -0,0 +1,56 @@
+﻿//**********************************************************************************
+// TourGuide Helper Function
+// John Gerdsen
+// 1/28/12
+//
+// Commonly used functions by TourGuide
+//
+//**********************************************************************************
+
+//Add a resize handler to window to make sure TourGuide stays centered
+$(document).ready(function () {
+    $(window).resize(function () {
+        // Make call to Utility Function
+        centerBox();
+    });
+});
+
+//Experimental
+//Captures keypressed allowing user to invoke by pressing 'h'
+$(document).keyup(function (e) {
+    if (e.keyCode == 72) {
+        $("#tourBox").toggle('slide', { direction: "up" }, 500);
+    }
+});
+
+//Function that does the math to make sure control is centered
+function centerBox() {
+    //Get the window height and width        
+    var winH = $(window).height();
+    var winW = $(window).width();
+    //Set the popup window to center
+    //ALREADY SET IN CSS        
+    //$("#tourControl").css('top', winH / 2 - $("#tourControl").height() / 2);
+    $("#tourBox").css('left', winW / 2 - $("#tourBox").width() / 2);
+    $("#tourBtn").css('left', winW / 2 - $("#tourBtn").width() / 2);
+}
+
+//Author: adameslinger.blogspot.com
+function addOnloadEvent(fnc) {
+    if (typeof window.addEventListener != "undefined")
+        window.addEventListener("load", fnc, false);
+    else if (typeof window.attachEvent != "undefined") {
+        window.attachEvent("onload", fnc);
+    }
+    else {
+        if (window.onload != null) {
+            var oldOnload = window.onload;
+            window.onload = function (e) {
+                oldOnload(e);
+                window[fnc]();
+            };
+        }
+        else
+            window.onload = fnc;
+    }
+}
